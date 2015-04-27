c978cd9393d24a82afc584b00d2835473d5ce763
AkitoshiAsakura <akitoshi.a@icloud.com>
2015-04-27 16:25:03 +0900

    no message

--- functions.cpp
+++ functions.cpp
@@ -30,12 +30,13 @@ int calendarFunctions::dayWeeksC (int year, int month) {
 
 int calendarFunctions::getYear() {
     int year = 0;
+    cout << "年を入力してください：" << endl;
+    cin >> year;
     for (bool yearIsNumber; yearIsNumber == false;) {
-        cout << "年を入力してください：" << endl;
-        cin >> year;
         if (cin.fail()) {
             yearIsNumber = false;
             cout << "数字を入力してください！" << endl;
+            scanf("%d", &year);
         } else {
             yearIsNumber = true;
         }
@@ -45,15 +46,17 @@ int calendarFunctions::getYear() {
 
 int calendarFunctions::getMonth() {
     int month = 0;
+    cout << "月を入力してください：" << endl;
+    cin >> month;
     for (bool monthIsCorrect = false; monthIsCorrect == false; ) {
-        cout << "月を入力してください：" << endl;
-        cin >> month;
         if (cin.fail()) {
             monthIsCorrect = false;
             cout << "数字を入力してください！" << endl;
+            cin >> month;
         } else if (month < 1 || month > 12) {
             monthIsCorrect = false;
             cout << "入力した月は存在しません。\n入力直してください。" << endl;
+            cin >> month;
         } else {
             monthIsCorrect = true;
         }
@@ -62,6 +65,50 @@ int calendarFunctions::getMonth() {
 }
 
 void calendarFunctions::outputCalendar (int year, int month, int dayPerMonth, int dayWeeks) {
+    int yearJapanese = 0;
+    int type;
+    if (year >= 1926) {
+        if (year == 1926) {
+            if (month != 12) {
+                type = 0;
+            } else {
+                type = 1;
+            }
+        } else if (year <= 1989) {
+            if (year == 1989) {
+                if (month == 1) {
+                    type = 2;
+                }
+            } else {
+                yearJapanese = year - 1926 + 1;
+                type = 3;
+            }
+        } else if (year > 1989) {
+            yearJapanese = year - 1989 + 1;
+            type = 4;
+        }
+        switch (type) {
+            case 0:
+                cout << "大正26年" << endl;
+                break;
+            case 1:
+                cout << "大正26年（12月25日まで）" << endl;
+                cout << "昭和元年（12月25日から）" << endl;
+                break;
+            case 2:
+                cout << "昭和64年（1月7日まで）" << endl;
+                cout << "平成元年（1月8日から）" << endl;
+                break;
+            case 3:
+                cout << "昭和" << yearJapanese << "年" << endl;
+                break;
+            case 4:
+                cout << "平成" << yearJapanese << "年" << endl;
+                break;
+            default:
+                break;
+        }
+    }
     cout << "西暦" << year << "年" << "　" << month << "月" << endl;
     cout << "--------------------------" << endl;
     cout << "日" << "  " << "月" << "　 " << "火" << "  " << "水" << "　 " << "木" << "  " << "金" << "　 " << "土" << endl;
